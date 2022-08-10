f260b4df52ef07d426755dac228da714f388ab26
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-08-10 15:51:23 +0530

    INit

--- src/pattern11.java
+++ src/pattern11.java
@@ -1,14 +1,24 @@
 public class pattern11 {
     public static void main(String[] args) {
-        int n=8;
+        int n=6;
+        int b=8;
 
-        for (int i=0;i<n;i++){
-            System.out.print("*");
-            for (int j=0;j==3;j++){
-                System.out.print("*");
+        for (int i=0;i<1;i++){
+            for (int j=0;j<=n;j++){
+            System.out.print(" " +"*");}
+        }
+        System.out.println();
+
+        for (int i=1;i<n;i++){
+            for(int j=1;j<b;j++){
+                System.out.print(" ");
             }
-            System.out.print(" ");
+            System.out.println("*");
+        }
 
+        for (int i=0;i<1;i++){
+            for (int j=0;j<=n;j++){
+                System.out.print(" " + "*");}
         }
 
     }
