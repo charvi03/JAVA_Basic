b7f5823d6aa187d44f6701fd25b6a54f6ed7e572
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-08-12 21:11:11 +0530

    INit

---
+++ src/array1.java
@@ -0,0 +1,13 @@
+public class array1 {
+    public static void main(String[] args) {
+        int[] marks = new int[5]; // int[] marks; marks = new int [5];
+        // int[]marks = {100,40,60,80,90}
+        marks[0] = 100; // array starts from zero
+        marks[1] = 40;
+        marks[2] = 60;
+        marks[3] = 80;
+        marks[4] = 90;
+        System.out.println(marks[2]);
+
+    }
+}
---
+++ src/practice1_array.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class practice1_array {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a;
+        System.out.println("Enter number of elements");
+        a= sc.nextInt();
+        int [] arr= new int[a];
+        System.out.println("Enter the elements");
+        for (int i=0;i<arr.length;i++){
+            arr[i]= sc.nextInt();
+            System.out.println(arr[i]);
+        }
+    }
+}
