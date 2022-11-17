c4298cf6ed5b01f81d9bbea8490c058a08cddf01
charvi03 <67728901+charvi03@users.noreply.github.com>
2022-11-17 21:26:53 +0530

    INIT

---
+++ src/basics/l_pattern.java
@@ -0,0 +1,14 @@
+package basics;
+
+public class l_pattern {
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++){
+            System.out.println("*");
+        }
+        for (int j=-1;j<=4;j++){
+            System.out.print("* ");
+        }
+
+        }
+    }
+
